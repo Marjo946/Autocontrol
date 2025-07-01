dd905ac9ecca087f57600c7a2bdfc5f94c2fe465
Marjo946 <arejonmedina@gmail.com>
2025-07-01 11:46:07 -0600

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -8,10 +8,10 @@ import matplotlib.pyplot as plt
 # Título del dashboard
 st.title("Dificultad para Autocontrol según el Género")
 
-# Cargar los datos
+# Cargar los datos desde archivo Excel
 @st.cache_data
 def cargar_datos():
-    return pd.read_csv("datos.csv")
+    return pd.read_excel("1.xlsx")
 
 df = cargar_datos()
 
